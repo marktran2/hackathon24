a655c11bbf04d785c7b7e5e744da440b6806c0d1
Mark Tran <z5421592@ad.unsw.edu.au>
2024-06-29 13:04:50 +1000

    Add navbar + dashboard

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+
+const Dashboard = () => {
+  return (
+    <>
+      <div className="h-screen w-screen bg-gray-100">
+        <Navbar />
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
