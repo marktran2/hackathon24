e5b704a1c65c13d5654eba3d7552ce7638e25500
Mark Quach <markquach@student.unsw.edu.au>
2024-06-30 11:20:33 +1000

    fixed the display for achievements

--- frontend/src/components/AchievementCard.jsx
+++ frontend/src/components/AchievementCard.jsx
@@ -10,19 +10,28 @@ const AchievementCard = ({
   const isComplete = numCompleted >= totalHabits;
 
   return (
-    <div onClick={openModal} className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6 transition ease-in-out delay-150 hover:-translate-y-1 cursor-pointer">
-      <img className="block mx-auto h-24 sm:mx-0 sm:shrink-0" src={image} alt="Achievement Symbol" />
-      <div className="text-center space-y-2 sm:text-left">
+    <div
+      onClick={openModal}
+      className="w-80 h-70 mx-auto bg-white rounded-xl shadow-lg flex flex-col items-center space-y-4 transition ease-in-out delay-150 hover:-translate-y-1 cursor-pointer"
+    >
+      <div className="flex justify-center items-center w-24 h-24 mt-4">
+        <img
+          className="object-cover w-full h-full"
+          src={image}
+          alt="Achievement Symbol"
+        />
+      </div>
+      <div className="text-center space-y-2 px-4">
         <div className="space-y-0.5">
           <p className="text-lg text-black font-semibold">
             {name}
           </p>
-            {isComplete ?
-              <p className="text-slate-500 font-medium">
-                {"Complete!"}
-              </p> :
-              <ProgressBar achievedGoals={numCompleted} totalGoals={totalHabits} />
-            }
+          {isComplete ?
+            <p className="text-slate-500 font-medium">
+              {"Complete!"}
+            </p> :
+            <ProgressBar achievedGoals={numCompleted} totalGoals={totalHabits} />
+          }
         </div>
       </div>
     </div>
